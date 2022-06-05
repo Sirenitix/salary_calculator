a138c56c20ae98c1ca4dc071f61badee725934fc
nx <suleevn@gmail.com>
2022-06-05 11:58:59 +0600

    Initial commit

---
+++ src/main/java/com/additional_project/salary_calculator/SalaryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.additional_project.salary_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryCalculatorApplication.class, args);
+	}
+
+}
