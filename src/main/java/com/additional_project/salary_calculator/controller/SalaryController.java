e09c130aadb59394a4a164e1e178bee6a28b8b82
nx <suleevn@gmail.com>
2022-06-05 17:56:19 +0600

    v2

--- src/main/java/com/additional_project/salary_calculator/controller/SalaryController.java
+++ src/main/java/com/additional_project/salary_calculator/controller/SalaryController.java
@@ -1,10 +1,18 @@
 package com.additional_project.salary_calculator.controller;
 
+import com.additional_project.salary_calculator.service.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SalaryController {
+    SalaryService salaryService;
+
+    @Autowired
+    public SalaryController(SalaryService salaryService) {
+        this.salaryService = salaryService;
+    }
 
     @GetMapping("/")
     public String salaryRequest(){
--- src/main/java/com/additional_project/salary_calculator/service/internal/SalaryServiceImpl.java
+++ src/main/java/com/additional_project/salary_calculator/service/internal/SalaryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.additional_project.salary_calculator.service.internal;
 
 import com.additional_project.salary_calculator.service.SalaryService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SalaryServiceImpl implements SalaryService {
     @Override
     public int calculate() {
