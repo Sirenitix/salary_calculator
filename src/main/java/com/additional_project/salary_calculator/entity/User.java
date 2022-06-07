b4ad957a4a136e387defeba94d5cd29d7325d4bb
nx <suleevn@gmail.com>
2022-06-07 17:44:17 +0600

    v5.1

---
+++ src/main/java/com/additional_project/salary_calculator/entity/User.java
@@ -0,0 +1,15 @@
+package com.additional_project.salary_calculator.entity;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class User {
+
+    private String login;
+    private String password;
+    private String role;
+    private Boolean enabled;
+
+}
