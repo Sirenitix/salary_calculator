c8d74c2eb84b28bf83d3f8ebb132507180cf5701
nx <suleevn@gmail.com>
2022-06-08 17:02:07 +0600

    v6

--- src/main/java/com/additional_project/salary_calculator/config/WebSecurityConfig.java
+++ src/main/java/com/additional_project/salary_calculator/config/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
-                .antMatchers("/signup").permitAll()
+                .antMatchers("/signup","/calculate","/about").permitAll()
                 .anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordParameter("password")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").and().exceptionHandling();
+                .logoutSuccessUrl("/login").and().exceptionHandling();
     }
 
     @Override
