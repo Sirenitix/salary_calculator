73069898eb799d222b4088e995a615256a7cfa16
nx <suleevn@gmail.com>
2022-06-19 23:37:38 +0600

    v24

---
+++ src/main/java/com/additional_project/salary_calculator/entity/ItemsToCalculateFull.java
@@ -0,0 +1,25 @@
+package com.additional_project.salary_calculator.entity;
+
+import lombok.*;
+
+@Data
+@ToString
+public class ItemsToCalculateFull extends ItemsToCalculate {
+    private int updateHour;
+    private int homeHour;
+    private int gymHour;
+    private int deepHour;
+    private int roomLook;
+    private int classMentor;
+    private int profHour;
+    private int noteHour;
+    private int noteType;
+    private int nisProgram;
+    private int qualityCategory;
+    private int ecoZone;
+    private int radZone;
+    private int inEnglish;
+    private int mentoring;
+    private int masters;
+
+}
